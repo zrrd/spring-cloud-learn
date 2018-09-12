933eb0d93ebdf5ac34b0997bcf303040738228b5
邵益炯 <shaoyijiong@worken.cn>
2018-09-12 20:44:23 +0800

    终于解决

---
+++ igame/src/main/java/cn/learn/igame/domain/repository/SteamMapper.java
@@ -0,0 +1,14 @@
+package cn.learn.igame.domain.repository;
+
+import cn.learn.igame.domain.Steam;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * .
+ *
+ * @author win
+ */
+public interface SteamMapper extends BaseMapper<Steam> {
+
+}
