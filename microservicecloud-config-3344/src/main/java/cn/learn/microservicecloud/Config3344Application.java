21774204382a5e11428b9699f78385afc4b81a19
邵益炯 <shaoyijiong@worken.cn>
2018-10-30 16:45:35 +0800

    添加配置中心

---
+++ microservicecloud-config-3344/src/main/java/cn/learn/microservicecloud/Config3344Application.java
@@ -0,0 +1,12 @@
+package cn.learn.microservicecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Config3344Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Config3344Application.class, args);
+  }
+}
