7be087eb70ec8d98b1dd302e18b02039cba51348
邵益炯 <shaoyijiong@worken.cn>
2018-12-11 10:15:22 +0800

    修改扫描方式

--- microservicecloud-provide-game-8002/src/main/java/cn/learn/microservicecloud/ProvideGame8002Application.java
+++ microservicecloud-provide-game-8002/src/main/java/cn/learn/microservicecloud/ProvideGame8002Application.java
@@ -1,8 +1,8 @@
 package cn.learn.microservicecloud;
 
-import cn.learn.igame.IgameApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -14,13 +14,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 //服务发现
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class,
+    scanBasePackages = "cn.learn.igame")
 public class ProvideGame8002Application {
 
   public static void main(String[] args) {
-    Class[] classes = new Class[2];
-    classes[0] = ProvideGame8002Application.class;
-    classes[1] = IgameApplication.class;
-    SpringApplication.run(classes, args);
+    SpringApplication.run(ProvideGame8002Application.class, args);
   }
 }
