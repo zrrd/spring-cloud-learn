ac02fba78619df745dd898a5376a6914bc6acbd9
邵益炯 <shaoyijiong@worken.cn>
2021-07-26 19:09:45 +0800

    dubbo + sentinel

---
+++ sentinel/sentinel-dubbo-consumer-2/src/main/java/learn/SentinelDubboConsumerApplication.java
@@ -0,0 +1,33 @@
+package learn;
+
+import cn.learn.HelloService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shaoyijiong
+ * @date 2021/7/20
+ */
+@SpringBootApplication
+public class SentinelDubboConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelDubboConsumerApplication.class, args);
+    }
+
+    @RestController
+    static class TestController {
+
+        @DubboReference(version = "1.0.0")
+        HelloService helloService;
+
+        @GetMapping("/test")
+        public String test() {
+            return helloService.hello("你好啊");
+        }
+    }
+
+}
